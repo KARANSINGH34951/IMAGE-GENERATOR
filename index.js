65e36556573bd28884334d7a880dd4b575365b4c
karan singh D <ceitkaransingh2026@gmail.com>
2024-02-18 17:16:16 +0530

    A new project to generate images on search

---
+++ index.js
@@ -0,0 +1,10 @@
+// let url=`https://api.unsplash.com/search/photos?pages=1&query=inida&client_id=YPAfQlqnyZmZ45wsfbe5re_wKlF2LtsqA5apSKJWEYQ`
+
+fetch(`https://api.unsplash.com/search/photos?pages=1&query=india&client_id=YPAfQlqnyZmZ45wsfbe5re_wKlF2LtsqA5apSKJWEYQ`)
+.then((res=>{
+  return res.json()
+}))
+.then((data)=>{
+  Object.entries(data)
+  console.log(data);
+})
