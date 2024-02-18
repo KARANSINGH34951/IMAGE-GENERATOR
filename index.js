c9d73de4e4e3046bbd395160084cfbfd1417794c
karan singh D <ceitkaransingh2026@gmail.com>
2024-02-18 23:17:58 +0530

    basic ui has been updated

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // let url=`https://api.unsplash.com/search/photos?pages=1&query=inida&client_id=YPAfQlqnyZmZ45wsfbe5re_wKlF2LtsqA5apSKJWEYQ`
 
-fetch(`https://api.unsplash.com/search/photos?pages=1&query=india&client_id=YPAfQlqnyZmZ45wsfbe5re_wKlF2LtsqA5apSKJWEYQ`)
+fetch(`https://api.unsplash.com/search/photos?pages=1&query=india flag&client_id=YPAfQlqnyZmZ45wsfbe5re_wKlF2LtsqA5apSKJWEYQ`)
 .then((res=>{
   return res.json()
 }))
 .then((data)=>{
   Object.entries(data)
   console.log(data);
+  console.log(data.results[0].urls.regular);
 })
