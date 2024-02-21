0db8157fcb43bbb5cd01b6e5138069abcd23e8d5
karan singh D <ceitkaransingh2026@gmail.com>
2024-02-21 20:22:26 +0530

    additional changes in api call

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 let form = document.querySelector("form")
 let input = document.getElementsByClassName("search")
 let section3 =document.getElementsByClassName("section-3") 
-let img1= document.getElementById("img1")
-let img2= document.getElementById("img2")
-let img3= document.getElementById("img3")
-let img4= document.getElementById("img4")
-let cardtext = document.getElementsByClassName("card-text")
+
+
 
 // let ramdom =`https://api.unsplash.com/photos/random/?client_id=YPAfQlqnyZmZ45wsfbe5re_wKlF2LtsqA5apSKJWEYQ`
 
@@ -24,40 +21,71 @@ let cardtext = document.getElementsByClassName("card-text")
 // function myfunction(){
 //   let src1; // Declare the src variable outside of the .then block
   
-//   fetch(`https://api.unsplash.com/photos/random/?client_id=YPAfQlqnyZmZ45wsfbe5re_wKlF2LtsqA5apSKJWEYQ`)
-//   .then((res=>{
-//     return res.json()
-//   }))
-//   .then((data)=>{
-//     Object.entries(data)
-//     console.log(data);
-//     console.log(data.urls.regular);
-//     src1=data.urls.regular; // Assign the value to src
-//     img1[0].src = src1;
-//   })
-// }
-
 document.addEventListener('DOMContentLoaded', function() {
-  // Get the image element by its ID
-  var imageElement = document.getElementById('img1');
+
+  let img1= document.getElementById("img1")  
+  let cardtext = document.getElementsByClassName("card-text")
+  let img2= document.getElementById("img2")  
+  let cardtext2 = document.getElementsByClassName("card-text2")
+  let img3= document.getElementById("img3")  
+  let cardtext3 = document.getElementsByClassName("card-text3")
+  let img4= document.getElementById("img4")  
+  let cardtext4 = document.getElementsByClassName("card-text4")
 
   fetch(`https://api.unsplash.com/photos/random/?client_id=YPAfQlqnyZmZ45wsfbe5re_wKlF2LtsqA5apSKJWEYQ`)
-          .then((res=>{
-            return res.json()
-          }))
-          .then((data)=>{
-            Object.entries(data)
-            console.log(data);
-            console.log(data.urls.regular);
-            console.log(data.
-              alt_description
-              );
-            
-            src1=data.urls.regular; // Assign the value to src
-            imageElement.src = src1; 
-            cardtext[0].innerHTML = data.alt_description; 
+.then((res=>{
+  return res.json()
+}))
+.then((data)=>{
+  Object.entries(data)
+  console.log(data);
+  console.log(data.urls.regular);
+  console.log(data.alt_description)
+  src1=data.urls.regular;
+  img1.src = src1; 
+  cardtext[0].innerHTML = data.alt_description; 
+  return fetch(`https://api.unsplash.com/photos/random/?client_id=YPAfQlqnyZmZ45wsfbe5re_wKlF2LtsqA5apSKJWEYQ`)
 })
-});
+.then((res=>{
+  return res.json()
+}))
+.then((data)=>{
+  Object.entries(data)
+  console.log(data);
+  console.log(data.urls.regular);
+  console.log(data.alt_description)
+  src2=data.urls.regular;
+  img2.src = src2; 
+  cardtext2[0].innerHTML = data.alt_description; 
+  return fetch(`https://api.unsplash.com/photos/random/?client_id=YPAfQlqnyZmZ45wsfbe5re_wKlF2LtsqA5apSKJWEYQ`)
+})
+.then((res=>{
+  return res.json()
+}))
+.then((data)=>{
+  Object.entries(data)
+  console.log(data);
+  console.log(data.urls.regular);
+  console.log(data.alt_description)
+  src3=data.urls.regular;
+  img3.src = src3; 
+  cardtext3[0].innerHTML = data.alt_description; 
+  return fetch(`https://api.unsplash.com/photos/random/?client_id=YPAfQlqnyZmZ45wsfbe5re_wKlF2LtsqA5apSKJWEYQ`)
+})
+.then((res=>{
+  return res.json()
+}))
+.then((data)=>{
+  Object.entries(data)
+  console.log(data);
+  console.log(data.urls.regular);
+  console.log(data.alt_description)
+  src4=data.urls.regular;
+  img4.src = src4; 
+  cardtext4[0].innerHTML = data.alt_description; 
+  
+})
+})
 
 
 
